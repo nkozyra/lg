13116dd7f3fe71478a3c074400ae7a9954afd8d3
Nathan Kozyra <nkozyra@gmail.com>
2016-03-15 23:58:28 -0400

    adds range #2

---
+++ Chapter4/examples/range.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"strconv"
+)
+
+var list []string
+
+func main() {
+	for i := 0; i < LENGTH; i++ {
+		v := "Entry #" + strconv.FormatInt(int64(i), 10)
+		list = append(list, v)
+	}
+
+	for j := range list {
+		log.Println(list[j])
+	}
+}
