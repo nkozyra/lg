c7f00dcaf89352f1446dbbbcd5945647ef166b60
Nathan Kozyra <nkozyra@gmail.com>
2016-03-15 23:59:41 -0400

    resets

--- Chapter4/examples/ch4-range.go
+++ Chapter4/examples/ch4-range.go
@@ -10,5 +10,10 @@ var list []string
 func main() {
 	for i := 0; i < LENGTH; i++ {
 		v := "Entry #" + strconv.FormatInt(int64(i), 10)
+		list = append(list, v)
+	}
+
+	for j := range list {
+		log.Println(list[j])
 	}
 }
